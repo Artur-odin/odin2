1f76d74dcf7c16fde6c07eb3246325d1da4abebe
Artur-odin <galeevartur1986@gmail.com>
2025-08-17 11:46:15 +0300

    точка ввхода 17.08.2025

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import my_module
 
-print("Hello World")
+#print("Hello World")
 
 print(my_module.summa(1,3))
+
+print(1, __name__)
+
+if __name__ == "__main__":
+    print("самостоятельный запуск")
+else:
+    print("был импортирован")
--- my_module.py
+++ my_module.py
@@ -1,4 +1,16 @@
-def summa(a, b):
+def summa(a, b) -> int:
+    """
+    Эта функция делает a + b
+    """
+
     return a + b
 
-print("привет из модуля")
+#print("привет из модуля")
+
+
+print(2, __name__)
+
+if __name__ == "__main__":
+    print("самостоятельный запуск")
+else:
+    print(__name__, "был импортирован")
